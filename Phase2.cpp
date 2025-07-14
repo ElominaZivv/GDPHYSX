ea794e54752ffbfa8b458f2dbd56e5aae4055a8d
pekep <zivv_elomina@dlsu.edu.ph>
2025-07-14 22:21:04 +0800

    Hotfix

--- Phase2.cpp
+++ Phase2.cpp
@@ -113,7 +113,7 @@ int main(void)
 
     for (int i = 0; i<5; i++)
     {
-        spheres[i]->setObjPos(particle_gap * (i-2), cableLength, 0.0f);
+        spheres[i]->setObjPos(particle_gap * (i-2), 0, 0.0f);
         spheres[i]->setMass(500.0f);
         spheres[i]->setRadius(particle_radius);
 
