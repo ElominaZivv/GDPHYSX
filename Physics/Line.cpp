c5e43368acaab5fa0b32234d02b3d57ee6cb4047
ChViv <168715281+ChViv@users.noreply.github.com>
2025-07-18 22:42:38 +0800

    vao sht

--- Physics/Line.cpp
+++ Physics/Line.cpp
@@ -7,17 +7,47 @@ Line::Line(Object* anchor, physics::P6Particle* p) {
 
     x2 = p->pos.x;
     y2 = p->pos.y;
+
+    initialize(); 
+    updateVertexData();
 }
 
 void Line::update(physics::P6Particle* p) {
 
     x2 = p->pos.x;
     y2 = p->pos.y;
+
+    updateVertexData();
 }
 
 void Line::draw() {
-    glBegin(GL_LINES);
-        glVertex2f(x1, y1);
-        glVertex2f(x2, y2);
-    glEnd();
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_LINES, 0, 2);
+    glBindVertexArray(0);
+}
+
+void Line::initialize() {
+    float vertices[4] = { x1, y1, x2, y2 };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), nullptr, GL_DYNAMIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void Line::updateVertexData() {
+    float vertices[4] = { x1, y1, x2, y2 };
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
--- Physics/Line.h
+++ Physics/Line.h
@@ -11,9 +11,13 @@ class Line {
 	public:
 		float x1, y1, x2, y2; //anchor and ball pos
 		float lineX1, lineX2, lineY1, lineY2;
+		GLuint VAO, VBO;
 
 		Line(Object* anchor, physics::P6Particle* p);
 		void update(physics::P6Particle* p);
 		void draw();
+	private:
+		void initialize();
+		void updateVertexData();
 
 };
